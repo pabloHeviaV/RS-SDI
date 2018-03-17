6781386b659c91a013a7158f1266957cf9334333
Sara Grimaldos <UO251782@uniovi.es>
2018-03-17 21:37:18 +0100

    Primera mitad de test

--- RSTests/src/com/uniovi/tests/RSTests.java
+++ RSTests/src/com/uniovi/tests/RSTests.java
@@ -16,6 +16,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_LoginView;
+import com.uniovi.tests.pageobjects.PO_Properties;
+import com.uniovi.tests.pageobjects.PO_RegisterView;
+import com.uniovi.tests.pageobjects.PO_View;
 import com.uniovi.tests.utils.SeleniumUtils;
 
 //Ordenamos las pruebas por el nombre del m�todo
@@ -49,11 +54,6 @@ public class RSTests {
 	// En Windows (Debe ser la versi�n 46.0 y desactivar las actualizacioens
 	// autom�ticas)):
 	static String PathFirefox = "C:\\Users\\Usuario\\Desktop\\Firefox46.win\\FirefoxPortable.exe";
-	// En MACOSX (Debe ser la versi�n 46.0 y desactivar las actualizaciones
-	// autom�ticas):
-	// static String PathFirefox =
-	// "/Applications/Firefox.app/Contents/MacOS/firefox-bin";
-	// Com�n a Windows y a MACOSX
 	static WebDriver driver = getDriver(PathFirefox);
 	static String URL = "http://localhost:8090";
 
@@ -62,6 +62,61 @@ public class RSTests {
 		System.setProperty("webdriver.firefox.bin", PathFirefox);
 		WebDriver driver = new FirefoxDriver();
 		return driver;
+	}
+
+	// 1.1 [RegVal] Registro de Usuario con datos válidos.
+	@Test
+	public void PR01() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		PO_RegisterView.fillForm(driver, "7@uniovi.es", "Pablo", "Menendez", "123456", "123456");
+		PO_LoginView.checkKey(driver, "users.message", PO_Properties.getSPANISH());
+	}
+
+	// 1.2 [RegInval] Registro de Usuario con datos inválidos (repetición de
+	// contraseña invalida).
+	@Test
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		PO_RegisterView.fillForm(driver, "8@uniovi.es", "Sara", "Grimaldos", "123456", "123453");
+		PO_RegisterView.checkKey(driver, "Error.signup.passwordConfirm.coincidence", PO_Properties.getSPANISH());
+	}
+
+	// 2.1 [InVal] Inicio de sesión con datos válidos.
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		PO_LoginView.fillForm(driver, "1@uniovi.es", "123456");
+		PO_LoginView.checkKey(driver, "users.message", PO_Properties.getSPANISH());
+	}
+
+	// 2.2 [InInVal] Inicio de sesión con datos inválidos (usuario no existente en
+	// la aplicación).
+	@Test
+	public void PR04() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		PO_LoginView.fillForm(driver, "9@uniovi.es", "123456");
+		PO_LoginView.checkKey(driver, "Error.login", PO_Properties.getSPANISH());
+	}
+
+	// 3.1 [LisUsrVal] Acceso al listado de usuarios desde un usuario en sesión.
+	@Test
+	public void PR05() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		PO_LoginView.fillForm(driver, "1@uniovi.es", "123456");
+		PO_LoginView.checkKey(driver, "users.message", PO_Properties.getSPANISH());
+		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id,'users-menu')]/a");
+		elementos.get(0).click();
+		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, 'user/list')]");
+		PO_LoginView.checkKey(driver, "list.users.description", PO_Properties.getSPANISH());
+	}
+
+	// [LisUsrInVal] Intento de acceso con URL desde un usuario no identificado al
+	// listado de usuarios desde un usuario en sesión.
+	// Debe producirse un acceso no permitido a vistas privadas.
+	@Test
+	public void PR06() {
+		driver.navigate().to("http://localhost:8090/user/list");
+		PO_LoginView.checkKey(driver, "login.message", PO_Properties.getSPANISH());
 	}
 
 }
